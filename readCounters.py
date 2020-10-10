1e3e705387939917b6bac51a3533a257236b266c
Tamas Szabo <szabogtamas@gmail.com>
2020-10-10 12:44:20 +0200

    Added NF node to count reads with FC

---
+++ readCounters.py
@@ -0,0 +1,20 @@
+class alignSingleEndReadsWithStar(nextflowProcess):
+    "Count reads with FeatureCounts, using general settings."
+
+    def customize_features(self):
+        self.inputs = [
+            "val manycpu from params.manycpu",
+            "val genomeannotation from params.genomeannotation",
+            'file alignedSams from aligned.collect()',
+        ]
+        self.outputs = ["file 'counts.tsv'"]
+        self.command = "featureCounts -T $manycpu\\\n            "
+        self.command += "-a $genomeannotation\\\n            "
+        self.command += "-o counts.tsv\\\n            "
+        self.command += "-${alignedSams.join(' ')\\\n"
+
+        self.manualDoc = self.__doc__
+        return None
+
+    def compile_command(self):
+        return self.command
