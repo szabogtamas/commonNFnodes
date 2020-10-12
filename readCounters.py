e87fe6d60c1d669db0146fd35ab6597e3255017e
Tamas Szabo <szabogtamas@gmail.com>
2020-10-12 11:23:28 +0200

    Added option to set count file name

--- readCounters.py
+++ readCounters.py
@@ -10,12 +10,13 @@ class countWithFeatureCounts(nextflowCmdProcess):
     def customize_features(self):
         self.inputs = [
             "val manycpu from params.manycpu",
+            "val count_file from params.count_file",
             "val genomeannotation from params.genomeannotation",
             "file alignedSams from aligned.collect()",
         ]
         self.outputs = ["file 'counts.tsv'"]
         self.command = "featureCounts -T $manycpu\\\n            "
         self.command += "-a $genomeannotation\\\n            "
-        self.command += "-o counts.tsv\\\n            "
+        self.command += "-o $count_file\\\n            "
         self.command += "${alignedSams.join(' ')}\\\n"
         return None
