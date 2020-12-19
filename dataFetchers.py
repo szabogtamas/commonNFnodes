cc9b28d269d3dab7cc69d4a224ac471df60db48b
Tamas Szabo <szabogtamas@gmail.com>
2020-12-19 14:05:27 +0100

    Fixed input channel name

--- dataFetchers.py
+++ dataFetchers.py
@@ -9,7 +9,7 @@ class downloadPairedSRA(nextflowCmdProcess):
 
     def customize_features(self):
         self.inputs = [
-            'tuple sample from params.sra_ids',
+            'tuple sample from params.sample_ids',
         ]
         self.outputs = ['tuple "${sample}_1.fastq", "${sample}_2.fastq", sample into insamples']
         self.command = "fastq-dump -I --split-files $sample"
