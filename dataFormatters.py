29433399a160725bb806383ae2a52a6ea933d71a
Tamas Szabo <szabogtamas@gmail.com>
2021-01-16 11:53:38 +0100

    Added hitlist creator nodes

--- dataFormatters.py
+++ dataFormatters.py
@@ -10,6 +10,10 @@ intro_folder = os.path.dirname(os.path.realpath(introSpect.__file__))
 rerun_preprocessing = True
 
 
+def safe_quote(s):
+    return '"' + s + '"'
+
+
 def meta_parser(sample_meta_file):
     return pd.read_csv(sample_meta_file, sep="\t").set_index("SampleID")
 
@@ -104,3 +108,132 @@ class countMatrixFormatter(nextflowProcess):
         self.rerun_preprocessing = node_params["rerun_preprocessing"]
         self.meta_parser = node_params["meta_parser"]
         return
+
+
+class hitListCreator(nextflowProcess):
+    def channel_pretreat(self):
+        return [
+            [
+                "de_tables",
+                "collect()",
+                "into{de_tabs; de_tabs2; de_tabs3}",
+            ],
+        ]
+
+    def dependencies(self):
+        return {
+            "imports": ["import os", "import pandas as pd"],
+            "helpers": [safe_quote],
+            "inhouse_packages": [self.intro_folder],
+        }
+
+    def channel_specifications(self):
+        return {
+            "de_tabs": chs("file", "de_tabs", "*de_tables"),
+            "hitlists": chs("file", "'*_hitlist.tsv'", None),
+        }
+
+    def process(
+        self,
+        edger_prefix: str,
+        *de_tables,
+    ) -> list:
+        """
+        Take topTag DE tables and create hitlists by selecting significantly UP or DOWN regulated genes.
+
+        Parameters
+        ----------
+        edger_prefix
+            Prefix of files added by EdgeR. This will be removed from labels.
+        de_tables
+            List of paths to de_tables created in the previous step.
+
+        Returns
+        -------
+        A list of hitlists that will be further analyzed for GO terms.
+        """
+
+        hls = []
+        bm = []
+        bms = []
+        de_tabs = []
+        for d in de_tables:
+            d = os.path.basename(d)
+            if d.find(edger_prefix) > -1:
+                if d != edger_prefix + "normalized_matrix.tsv":
+                    de_tabs.append(d)
+        for tbl in de_tabs:
+            tbn = (
+                tbl.replace(edger_prefix, "").replace(".tsv", "").replace("___", "_")
+            )  # , "/")
+            df = pd.read_csv(tbl, sep="\t")
+            upreg = ",".join(
+                df.loc[
+                    (df["logFC"] > 1) & (df["FDR"] < 0.05) & df["Symbol"].notna(),
+                    "Symbol",
+                ].astype(str)
+            )
+            downreg = ",".join(
+                df.loc[
+                    (df["logFC"] < -1) & (df["FDR"] < 0.05) & df["Symbol"].notna(),
+                    "Symbol",
+                ].astype(str)
+            )
+            hl = tbn + "_UP," + upreg + ":" + tbn + "_DOWN," + downreg
+            hls.append([tbn, hl])
+            bm.append(hl)
+            bms.append(",".join([tbn, upreg, downreg]))
+        hls.append(["overview_by_regulation", ":".join(bm)])
+        hls.append(["overview_by_condition", ":".join(bms)])
+        for i, h in enumerate(hls):
+            h = [safe_quote(x) for x in h]
+            with open("n" + str(i) + "_hitlist.tsv", "w") as f:
+                f.write("\t".join(h))
+        return None
+
+
+class gseaTabCreator(nextflowProcess):
+    def dependencies(self):
+        return {
+            "imports": ["import os", "import pandas as pd"],
+            "helpers": [safe_quote],
+            "inhouse_packages": [self.intro_folder],
+        }
+
+    def channel_specifications(self):
+        return {
+            "de_tabs2": chs("file", "de_tabs", "*de_tables"),
+            "gsea_tabs": chs("stdout", "", None),
+        }
+
+    def process(
+        self,
+        edger_prefix: str,
+        *de_tables,
+    ) -> list:
+        """
+        Take topTag DE tables and label them for GSEA.
+
+        Parameters
+        ----------
+        edger_prefix
+            Prefix of files added by EdgeR. This will be removed from labels.
+        de_tables
+            List of paths to de_tables created in the previous step.
+
+        Returns
+        -------
+        An expression with condition labels and table paths to be fed into GSEA script.
+        """
+
+        de_tabs = []
+        gsea_in = ""
+        for d in de_tables:
+            d = os.path.basename(d)
+            if d.find(edger_prefix) > -1:
+                if d != edger_prefix + "normalized_matrix.tsv":
+                    de_tabs.append(d)
+        for tbl in de_tabs:
+            tbn = tbl.replace(edger_prefix, "").replace(".tsv", "").replace("___", "/")
+            gsea_in += tbn + ":" + os.path.abspath(tbl) + ","
+        return '"' + gsea_in[:-1] + '"'
--- rmdCreators.py
+++ rmdCreators.py
@@ -7,7 +7,7 @@ chs = introSpect.flowNodes.createChannelSpecification
 intro_folder = os.path.dirname(os.path.realpath(introSpect.__file__))
 
 
-class notebookEdger(nextflowProcess):
+class notebookTemplater(nextflowProcess):
     "Render Rmd notebooks to facilitate editing of specific figures."
 
     def dependencies(self):
@@ -19,6 +19,18 @@ class notebookEdger(nextflowProcess):
 
     def directives(self):
         return {"publishDir": "'../notebooks', mode: 'copy'"}
+
+    def customize_features(self):
+        node_params = dict(
+            intro_folder=intro_folder,
+        )
+        node_params.update(self.node_params)
+        self.intro_folder = node_params["intro_folder"]
+        return
+
+
+class notebookEdger(notebookTemplater):
+    "Render Rmd notebooks to facilitate editing of specific figures."
 
     def channel_specifications(self):
         return {
@@ -66,10 +78,253 @@ class notebookEdger(nextflowProcess):
             nb = jinja2.Template(f.read())
         return nb.render(nb_kws)
 
-    def customize_features(self):
-        node_params = dict(
-            intro_folder=intro_folder,
+
+class notebookProgeny(notebookTemplater):
+    "Render Rmd notebooks to facilitate editing of specific figures."
+
+    def channel_specifications(self):
+        return {
+            "progeny_notebook_pars": chs(
+                "tuple",
+                (
+                    "progeny_nb_template",
+                    "progeny_script_path",
+                    "file(progeny_counts)",
+                    "progeny_condition_order",
+                    "species",
+                ),
+                (
+                    "progeny_nb_template",
+                    "progeny_script_path",
+                    "progeny_counts",
+                    "progeny_condition_order",
+                    "species",
+                ),
+            ),
+            "progeny_notebook": chs("file", "'progeny_paths.Rmd'", "outFile"),
+        }
+
+    def process(
+        self,
+        progeny_nb_template,
+        progeny_script_path,
+        progeny_counts,
+        progeny_condition_order,
+        progeny_condition_colors,
+        species,
+        nb_title=None,
+        nb_project_dir=None,
+    ):
+        if nb_title in [None, "None", ""]:
+            nb_title = "Pathway perturbation scores from PROGENy"
+        if nb_project_dir in [None, "None", ""]:
+            nb_project_dir = "getwd()"
+        nb_kws = dict(
+            nb_title=nb_title,
+            nb_project_dir=nb_project_dir,
+            progeny_script_path="pipeline/bin/" + os.path.basename(progeny_script_path),
+            progeny_counts="tables/" + os.path.basename(progeny_counts),
+            progeny_condition_order=progeny_condition_order,
+            progeny_condition_colors=progeny_condition_colors,
+            progeny_species=species,
         )
-        node_params.update(self.node_params)
-        self.intro_folder = node_params["intro_folder"]
-        return
+        with open(progeny_nb_template) as f:
+            nb = jinja2.Template(f.read())
+        return nb.render(nb_kws)
+
+
+class notebookGO(notebookTemplater):
+    "Render Rmd notebooks to facilitate editing of specific figures."
+
+    def channel_specifications(self):
+        return {
+            "go_notebook_pars": chs(
+                "tuple",
+                (
+                    "go_nb_template",
+                    "go_script_path",
+                    "go_hitlists",
+                    "order_of_conditions",
+                    "go_tag",
+                    "go_prefix",
+                    "species",
+                    "mcat",
+                    "msubcat",
+                ),
+                (
+                    "go_nb_template",
+                    "go_script_path",
+                    "go_hitlists",
+                    "go_condition_order",
+                    None,
+                    None,
+                    "go_species",
+                    "go_maincat",
+                    "go_subcat",
+                ),
+            ),
+            "hitlist_notebook": chs(
+                "file",
+                '"${go_prefix}${go_tag.replace(' + "'\\\"', ''" + ')}.Rmd"',
+                "outFile",
+            ),
+        }
+
+    def process(
+        self,
+        go_nb_template,
+        go_script_path,
+        go_hitlists,
+        go_condition_order,
+        nb_title=None,
+        nb_project_dir=None,
+        go_species=None,
+        go_maincat=None,
+        go_subcat=None,
+    ):
+        if nb_title in [None, "None", ""]:
+            nb_title = "GO terms associated to DE genes RNASeq data"
+        if nb_project_dir in [None, "None", ""]:
+            nb_project_dir = "dirname(getwd())"
+        if go_species in [None, "None", ""]:
+            go_species = "Mus musculus"
+        if go_maincat in [None, "None", ""]:
+            go_maincat = "C5"
+        if go_subcat in [None, "None", ""]:
+            go_subcat = "BP"
+        nb_kws = dict(
+            nb_title=nb_title,
+            nb_project_dir=nb_project_dir,
+            go_script_path="pipeline/bin/" + os.path.basename(go_script_path),
+            go_hitlists=go_hitlists,
+            go_condition_order=go_condition_order,
+            go_species=go_species,
+            go_maincat=go_maincat,
+            go_subcat=go_subcat,
+        )
+        with open(go_nb_template) as f:
+            nb = jinja2.Template(f.read())
+        return nb.render(nb_kws)
+
+
+class notebookGSEA(notebookTemplater):
+    "Render Rmd notebooks to facilitate editing of specific figures."
+
+    def process(
+        self,
+        gsea_nb_template,
+        gsea_script_path,
+        gsea_input_tables,
+        gsea_condition_order,
+        nb_title=None,
+        nb_project_dir=None,
+        gsea_species=None,
+        gsea_maincat=None,
+        gsea_subcat=None,
+    ):
+        if nb_title in [None, "None", ""]:
+            nb_title = "GSEA of RNASeq data"
+        if nb_project_dir in [None, "None", ""]:
+            nb_project_dir = "getwd()"
+        if gsea_species in [None, "None", ""]:
+            gsea_species = "Mus musculus"
+        if gsea_maincat in [None, "None", ""]:
+            gsea_maincat = "H"
+        if gsea_subcat in [None, "None", ""]:
+            gsea_subcat = "NULL"
+
+        if isinstance(gsea_input_tables, str):
+            gsea_input_tables = gsea_input_tables.split(",")
+
+        nb_kws = dict(
+            nb_title=nb_title,
+            nb_project_dir=nb_project_dir,
+            gsea_script_path="pipeline/bin/" + os.path.basename(gsea_script_path),
+            gsea_input_tables=",".join(
+                [
+                    x.split(":")[0]
+                    + ":"
+                    + "tables/"
+                    + os.path.basename(x.split(":")[1])
+                    for x in gsea_input_tables
+                ]
+            ),
+            gsea_condition_order=gsea_condition_order,
+            gsea_species=gsea_species,
+            gsea_maincat=gsea_maincat,
+            gsea_subcat=gsea_subcat,
+        )
+
+        with open(gsea_nb_template) as f:
+            nb = jinja2.Template(f.read())
+        return nb.render(nb_kws)
+
+
+class notebookPositional(notebookTemplater):
+    "Render Rmd notebooks to facilitate editing of specific figures."
+
+    def channel_specifications(self):
+        return {
+            "positional_notebook_pars": chs(
+                "tuple",
+                (
+                    "positional_nb_template",
+                    "positional_script_path",
+                    "positional_tabs",
+                    "order_of_conditions",
+                    "positional_prefix",
+                    "species",
+                ),
+                (
+                    "positional_nb_template",
+                    "positional_script_path",
+                    "positional_input_tables",
+                    "positional_condition_order",
+                    None,
+                    "gsea_species",
+                ),
+            ),
+            "positional_notebook": chs("file", '"${positional_prefix}.Rmd"', "outFile"),
+        }
+
+    def process(
+        self,
+        positional_nb_template,
+        positional_script_path,
+        positional_input_tables,
+        positional_condition_order,
+        nb_title=None,
+        nb_project_dir=None,
+        gsea_species=None,
+    ):
+        if nb_title in [None, "None", ""]:
+            nb_title = "Overrrepresented chromosomal positions"
+        if nb_project_dir in [None, "None", ""]:
+            nb_project_dir = "getwd()"
+        if gsea_species in [None, "None", ""]:
+            gsea_species = "Mus musculus"
+
+        if isinstance(positional_input_tables, str):
+            positional_input_tables = positional_input_tables.split(",")
+
+        nb_kws = dict(
+            nb_title=nb_title,
+            nb_project_dir=nb_project_dir,
+            positional_script_path="pipeline/bin/"
+            + os.path.basename(positional_script_path),
+            positional_input_tables=",".join(
+                [
+                    x.split(":")[0]
+                    + ":"
+                    + "tables/"
+                    + os.path.basename(x.split(":")[1])
+                    for x in positional_input_tables
+                ]
+            ),
+            positional_condition_order=positional_condition_order,
+            gsea_species=gsea_species,
+        )
+
+        with open(positional_nb_template) as f:
+            nb = jinja2.Template(f.read())
+        return nb.render(nb_kws)
