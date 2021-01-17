a7a221ba1135778a3b4fc117db5c09497b15d4b9
Tamas Szabo <szabogtamas@gmail.com>
2021-01-17 10:43:54 +0100

    Added option to switch between fetching

--- checkersAndTrimmers.py
+++ checkersAndTrimmers.py
@@ -1,19 +1,31 @@
 from .nodePrototypes import nextflowCmdProcess
 
+wait_to_fetch = False
+
 
 class rnaBulkTrimmomaticSE(nextflowCmdProcess):
     "Trim Single End reads with Trimmomatic."
 
     def channel_pretreat(self):
-        return [
-            [
-                "Channel",
-                "from([params.raw_fastqs, params.sample_ids].transpose())",
-                "set{insamples}",
-            ],
-        ]
+        if self.wait_to_fetch:
+            p = dict()
+        else:
+            p = [
+                [
+                    "Channel",
+                    "from([params.raw_fastqs, params.sample_ids].transpose())",
+                    "map{[prams.input_folder + '/' + it[0], it[1]]}",
+                    "set{insamples}",
+                ],
+            ]
+        return p
 
     def customize_features(self):
+        node_params = dict(
+            wait_to_fetch=wait_to_fetch,
+        )
+        node_params.update(self.node_params)
+        self.wait_to_fetch = node_params["wait_to_fetch"]
         self.inputs = [
             "val adapterFileIllumina from params.adapterFileIllumina",
             "val manycpu from params.manycpu",
@@ -21,7 +33,7 @@ class rnaBulkTrimmomaticSE(nextflowCmdProcess):
             "tuple raw, sample from insamples",
         ]
         self.outputs = ['tuple sample, "${sample}_trimed.fastq" into trimmed_fastqs']
-        self.command = "trimmomatic SE ${indir}/${raw}\\\n            "
+        self.command = "trimmomatic SE $raw\\\n            "
         self.command += "${sample}_trimed.fastq\\\n            "
         self.command += "ILLUMINACLIP:${adapterFileIllumina}:2:30:10 LEADING:3 TRAILING:3 MINLEN:36\\\n            "
         self.command += "-threads $manycpu\n"
@@ -33,15 +45,25 @@ class rnaBulkTrimmomaticPE(nextflowCmdProcess):
     "Trim Paired End reads with Trimmomatic."
 
     def channel_pretreat(self):
-        return [
-            [
-                "Channel",
-                "from([params.forward_fastqs, params.reverse_fastqs, params.sample_ids].transpose())",
-                "set{insamples}",
-            ],
-        ]
+        if self.wait_to_fetch:
+            p = dict()
+        else:
+            p = [
+                [
+                    "Channel",
+                    "from([params.forward_fastqs, params.reverse_fastqs, params.sample_ids].transpose())",
+                    "map{[prams.input_folder + '/' + it[0], prams.input_folder + '/' + it[1], it[2]]}",
+                    "set{insamples}",
+                ],
+            ]
+        return p
 
     def customize_features(self):
+        node_params = dict(
+            wait_to_fetch=wait_to_fetch,
+        )
+        node_params.update(self.node_params)
+        self.wait_to_fetch = node_params["wait_to_fetch"]
         self.inputs = [
             "val adapterFileIllumina from params.adapterFileIllumina",
             "val manycpu from params.manycpu",
@@ -54,30 +76,6 @@ class rnaBulkTrimmomaticPE(nextflowCmdProcess):
         self.command = (
             "trimmomatic PE ${indir}/${forward} ${indir}/${reverse}\\\n            "
         )
-        self.command += (
-            "${sample}_trim_1.fastq ${sample}_forward_unpaired.fastq\\\n            "
-        )
-        self.command += (
-            "${sample}_trim_2.fastq ${sample}_reverse_unpaired.fastq\\\n            "
-        )
-        self.command += "ILLUMINACLIP:$adapterFileIllumina:2:30:10:8:keepBothReads LEADING:3 TRAILING:3 MINLEN:36\\\n            "
-        self.command += "-threads $manycpu\n"
-        return None
-
-
-class rnaBulkSRATrimmomaticPE(nextflowCmdProcess):
-    "Trim Paired End reads with Trimmomatic."
-
-    def customize_features(self):
-        self.inputs = [
-            "val adapterFileIllumina from params.adapterFileIllumina",
-            "val manycpu from params.manycpu",
-            "tuple forward, reverse, sample from insamples",
-        ]
-        self.outputs = [
-            'tuple sample, "${sample}_trim_1.fastq", "${sample}_trim_2.fastq" into trimmed_fastqs'
-        ]
-        self.command = "trimmomatic PE $forward $reverse\\\n            "
         self.command += (
             "${sample}_trim_1.fastq ${sample}_forward_unpaired.fastq\\\n            "
         )
--- dataFetchers.py
+++ dataFetchers.py
@@ -14,8 +14,58 @@ class downloadPairedSRA(nextflowCmdProcess):
         self.outputs = [
             'tuple "${sample}_1.fastq", "${sample}_2.fastq", sample into insamples'
         ]
-        self.command = "fasterq-dump -I -t ./tmp --split-files $sample"
+        self.command = "fastq-dump -I -O ./tmp -X 500 --split-files --disable-multithreading $sample\n            "
+        self.command += "mv tmp/${sample}_1.fastq ${sample}_1.fastq\n            "
+        self.command += "mv tmp/${sample}_2.fastq ${sample}_2.fastq"
         return None
+
+
+class downloadPairedSRAfast(nextflowCmdProcess):
+    "Download archive sequencing data from SRA database."
+
+    def directives(self):
+        return {"publishDir": "params.input_folder", "label": "'manycpu'"}
+
+    def customize_features(self):
+        self.inputs = [
+            "val manycpu from params.manycpu",
+            "tuple sample from params.sample_ids",
+        ]
+        self.outputs = [
+            'tuple "${sample}_1.fastq", "${sample}_2.fastq", sample into insamples'
+        ]
+        self.command = "fasterq-dump $sample --split-files --threads $manycpu -t ./tmp\n            "
+        self.command += "mv tmp/${sample}_1.fastq ${sample}_1.fastq\n            "
+        self.command += "mv tmp/${sample}_2.fastq ${sample}_2.fastq"
+        return None
+
+
+class downloadPairedSRAbuiltin(nextflowCmdProcess):
+    "Download archive sequencing data from SRA database."
+
+    def directives(self):
+        return {"publishDir": "params.input_folder"}
+
+    def channel_pretreat(self):
+        return [
+            [
+                "Channel",
+                "fromSRA(params.sample_ids)",
+                "set{sra_samples}",
+            ],
+        ]
+
+    def customize_features(self):
+        self.inputs = [
+            "val indir from params.input_folder",
+            "tuple sample, fq from sra_samples",
+        ]
+        self.outputs = [
+            'tuple "${fq[0]}", "${fq[1]}", sample into insamples' "stdout sra_succes"
+        ]
+        self.command += "mv ${fq[0]} ${indir}/${fq[0]}\n            "
+        self.command += "mv ${fq[1]} ${indir}/${fq[1]}\n            "
+        self.command = "echo $sample"
 
 
 class localFetchNunzip(nextflowCmdProcess):
