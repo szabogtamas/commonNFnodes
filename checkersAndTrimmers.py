ae8b3681dc10305c1d800f4ac3507ef96dec68a7
Tamas Szabo <szabogtamas@gmail.com>
2021-01-23 12:46:39 +0100

    Added Cutadapt to trimmers

--- checkersAndTrimmers.py
+++ checkersAndTrimmers.py
@@ -81,3 +81,42 @@ class rnaBulkTrimmomaticPE(nextflowCmdProcess):
         self.command += "ILLUMINACLIP:$adapterFileIllumina:2:30:10:8:keepBothReads LEADING:3 TRAILING:3 MINLEN:36\\\n            "
         self.command += "-threads $manycpu\n"
         return None
+
+
+class rnaBulkCutadaptSE(nextflowCmdProcess):
+    "Trim Single End reads with Cutadapt."
+
+    def channel_pretreat(self):
+        if self.wait_to_fetch:
+            p = dict()
+        else:
+            p = [
+                [
+                    "Channel",
+                    "from([params.raw_fastqs, params.sample_ids].transpose())",
+                    "map{[params.input_folder + '/' + it[0], it[1]]}",
+                    "set{insamples}",
+                ],
+            ]
+        return p
+
+    def customize_features(self):
+        node_params = dict(
+            wait_to_fetch=wait_to_fetch,
+        )
+        node_params.update(self.node_params)
+        self.wait_to_fetch = node_params["wait_to_fetch"]
+        self.inputs = [
+            "val adapterSequence from params.adapterSequence",
+            "val manycpu from params.manycpu",
+            "tuple raw, sample from insamples",
+        ]
+        self.outputs = ['tuple sample, "${sample}_trimed.fastq" into trimmed_fastqs']
+        self.command = "cutadapt $raw\\\n            "
+        self.command += "--cores=$manycpu\n            "
+        self.command += "-a $adapterSequence\\\n            "
+        self.command += "-o ${sample}_trimed.fastq\\\n            "
+        self.command += "-m 8\\\n            "
+        self.command += "$raw\\\n"
+
+        return None
