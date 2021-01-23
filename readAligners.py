decd35bee608c460894365a071eb92784f85b76d
Tamas Szabo <szabogtamas@gmail.com>
2021-01-23 14:16:31 +0100

    Added Hisat2 aligner

--- readAligners.py
+++ readAligners.py
@@ -92,7 +92,6 @@ class alignSingleEndReadsWithBWA(nextflowCmdProcess):
         ]
         self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
         self.command = "bwa aln\\\n            "
-        self.command += "--o 0\\\n            "
         self.command += "-t $manycpu\\\n            "
         self.command += "$genomeindex\\\n            "
         self.command += "${sample}_trimed.fastq\\\n            "
@@ -117,6 +116,7 @@ class alignSingleEndSmallRNAReadsWithBWA(nextflowCmdProcess):
         ]
         self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
         self.command = "bwa aln\\\n            "
+        self.command += "--o 0\\\n            "
         self.command += "-t $manycpu\\\n            "
         self.command += "$genomeindex\\\n            "
         self.command += "${sample}_trimed.fastq\\\n            "
@@ -126,4 +126,43 @@ class alignSingleEndSmallRNAReadsWithBWA(nextflowCmdProcess):
         self.command += "${sample}.sai\n            "
         self.command += "${sample}_trimed.fastq\\\n            "
         self.command += "> ${sample}_\\\n            "
+        return None
+
+
+class alignSingleEndReadsWithHisat2(nextflowCmdProcess):
+    "Use Hisat2 to align single end reads with general settings."
+
+    def customize_features(self):
+        self.inputs = [
+            "val manycpu from params.manycpu",
+            "val genomedir from params.genomedir",
+            "val genomeindex from params.genomeindex",  # e.g. /data/genomes/hg38/index/HiSat2/ucsc/hg38refGene/hg38refGene or indices/hisat/ucsc/hg38_tran/genome_tran
+            'tuple sample, "${sample}_trimed.fastq" from trimmed_fastqs',
+        ]
+        self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
+        self.command = "hisat2 -q\\\n            "
+        self.command += "-x $genomeindex\\\n            "
+        self.command += "-U ${sample}_trimed.fastq\\\n            "
+        self.command += "-S ${sample}_Aligned.out.sam\\\n            "
+        self.command += "-p $manycpu\\\n"
+        return None
+
+
+class alignSingleEndSmallRNAReadsWithHisat2(nextflowCmdProcess):
+    "Use Hisat2 to align single end reads with settings optimized for miRNA."
+
+    def customize_features(self):
+        self.inputs = [
+            "val manycpu from params.manycpu",
+            "val genomedir from params.genomedir",
+            "val genomeindex from params.genomeindex",  # e.g. /data/genomes/hg38/index/bwa/ucsc/hg38.fa
+            'tuple sample, "${sample}_trimed.fastq" from trimmed_fastqs',
+        ]
+        self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
+        self.command = "hisat2 -q\\\n            "
+        self.command += "--end-to-end --very-sensitive\\\n            "
+        self.command += "-x $genomeindex\\\n            "
+        self.command += "-U ${sample}_trimed.fastq\\\n            "
+        self.command += "-S ${sample}_Aligned.out.sam\\\n            "
+        self.command += "-p $manycpu\\\n"
         return None
