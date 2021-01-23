d51fb9acec22db73afe0258bd80791d9bd8d52c3
Tamas Szabo <szabogtamas@gmail.com>
2021-01-23 13:02:29 +0100

    Added Bowtie2 to aligners

--- readAligners.py
+++ readAligners.py
@@ -41,6 +41,45 @@ class alignSingleEndReadsWithStar(nextflowCmdProcess):
         return None
 
 
+class alignSingleEndReadsWithBowtie2(nextflowCmdProcess):
+    "Use Bowtie2 to align single end reads with general settings."
+
+    def customize_features(self):
+        self.inputs = [
+            "val manycpu from params.manycpu",
+            "val genomedir from params.genomedir",
+            "val genomeindex from params.genomeindex",  # e.g. /data/genomes/hg38/index/bowtie2/ucsc/hg38
+            'tuple sample, "${sample}_trimed.fastq" from trimmed_fastqs',
+        ]
+        self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
+        self.command = "bowtie2 -q\\\n            "
+        self.command += "-x $genomeindex\\\n            "
+        self.command += "-U ${sample}_trimed.fastq\\\n            "
+        self.command += "-S ${sample}_Aligned.out.sam\\\n            "
+        self.command += "-p $manycpu\\\n"
+        return None
+
+
+class alignSingleEndSmallRNAReadsWithBowtie2(nextflowCmdProcess):
+    "Use Bowtie2 to align single end reads with settings optimized for miRNA."
+
+    def customize_features(self):
+        self.inputs = [
+            "val manycpu from params.manycpu",
+            "val genomedir from params.genomedir",
+            "val genomeindex from params.genomeindex",  # e.g. /data/genomes/hg38/index/bowtie2/ucsc/hg38
+            'tuple sample, "${sample}_trimed.fastq" from trimmed_fastqs',
+        ]
+        self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
+        self.command = "bowtie2 -q\\\n            "
+        self.command += "--very-sensitive-local\\\n            "
+        self.command += "-x $genomeindex\\\n            "
+        self.command += "-U ${sample}_trimed.fastq\\\n            "
+        self.command += "-S ${sample}_Aligned.out.sam\\\n            "
+        self.command += "-p $manycpu\\\n"
+        return None
+
+
 class alignSingleEndReadsWithBWA(nextflowCmdProcess):
     "Use BWA to align single end reads with general settings."
 
@@ -48,7 +87,7 @@ class alignSingleEndReadsWithBWA(nextflowCmdProcess):
         self.inputs = [
             "val manycpu from params.manycpu",
             "val genomedir from params.genomedir",
-            "val genomeindex from params.genomeindex",
+            "val genomeindex from params.genomeindex",  # e.g. /data/genomes/hg38/index/bwa/ucsc/hg38.fa
             'tuple sample, "${sample}_trimed.fastq" from trimmed_fastqs',
         ]
         self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
@@ -62,7 +101,7 @@ class alignSingleEndReadsWithBWA(nextflowCmdProcess):
         self.command += "$genomeindex\\\n            "
         self.command += "${sample}.sai\n            "
         self.command += "${sample}_trimed.fastq\\\n            "
-        self.command += "> ${sample}_\\\n            "
+        self.command += "> ${sample}_Aligned.out.sam\\\n            "
         return None
 
 
@@ -73,7 +112,7 @@ class alignSingleEndSmallRNAReadsWithBWA(nextflowCmdProcess):
         self.inputs = [
             "val manycpu from params.manycpu",
             "val genomedir from params.genomedir",
-            "val genomeindex from params.genomeindex",
+            "val genomeindex from params.genomeindex",  # e.g. /data/genomes/hg38/index/bwa/ucsc/hg38.fa
             'tuple sample, "${sample}_trimed.fastq" from trimmed_fastqs',
         ]
         self.outputs = ['file "${sample}_Aligned.out.sam" into aligned']
